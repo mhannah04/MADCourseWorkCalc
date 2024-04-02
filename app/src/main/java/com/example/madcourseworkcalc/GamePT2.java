3f87fc8498759d517eef93ab0b9039f4aff66121
Max Hannah <max.hannah4@outlook.com>
2024-04-02 17:36:59 +0100

    Leader board functional, now player will be added to top spot in leaderboard, upon entering a name

--- app/src/main/java/com/example/madcourseworkcalc/GamePT2.java
+++ app/src/main/java/com/example/madcourseworkcalc/GamePT2.java
@@ -7,24 +7,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Random;
+import java.text.DecimalFormat;
 
 public class GamePT2 extends AppCompatActivity {
+
+    TextView name1, name2, name3, name4, name5;
+    TextView number1, number2, number3, number4, number5;
+    TextView time1, time2, time3, time4, time5;
+    TextView date1, date2, date3, date4, date5;
+
+
+
+
     TextView Timer;
     ImageView imageView3;
     ProgressBar progressBar;
@@ -37,15 +53,23 @@ public class GamePT2 extends AppCompatActivity {
     private long elapsedTime = 0;
     int progressBarInt =0;
 
+    RelativeLayout relativeLayout;
+    EditText enterName;
+    Button submitNameButton;
+    ImageButton closeLayout;
+
     CountDownTimer countDownTimer;
     CountDownTimer countUpTimer;
     TextView questionBox;
+
+    String[][] matrix = {{"","","",""},{"","","",""},{"","","",""},{"","","",""},{"","","",""}};
 
     ArrayList <Integer> numbers = new ArrayList<Integer>();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_pt2);
 
@@ -57,7 +81,37 @@ public class GamePT2 extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
         imageView3 = findViewById(R.id.imageView3);
         scoresTable = findViewById(R.id.scoresTable);
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        relativeLayout = findViewById(R.id.submitName);
+        enterName = findViewById(R.id.enterName);
+        submitNameButton = findViewById(R.id.submitNameButton);
+        closeLayout = findViewById(R.id.imageButton);
+
+        name1 = findViewById(R.id.nameOne);
+        name2 = findViewById(R.id.nameTwo);
+        name3 = findViewById(R.id.nameThree);
+        name4 = findViewById(R.id.nameFour);
+        name5 = findViewById(R.id.nameFive);
+
+        number1 = findViewById(R.id.numbOne);
+        number2 = findViewById(R.id.numbTwo);
+        number3 = findViewById(R.id.numbThree);
+        number4 = findViewById(R.id.numbFour);
+        number5 = findViewById(R.id.numbFive);
+
+        time1 = findViewById(R.id.timeOne);
+        time2 = findViewById(R.id.timeTwo);
+        time3 = findViewById(R.id.timeThree);
+        time4 = findViewById(R.id.timeFour);
+        time5 = findViewById(R.id.timeFive);
+
+        date1 = findViewById(R.id.dateOne);
+        date2 = findViewById(R.id.dateTwo);
+        date3 = findViewById(R.id.dateThree);
+        date4 = findViewById(R.id.dateFour);
+        date5 = findViewById(R.id.dateFive);
+
+
+
 
         for (int i = 0; i < 12; i++) {
             numbers.add(i + 1);
@@ -107,10 +161,56 @@ public class GamePT2 extends AppCompatActivity {
 
                         submitButton.setVisibility(View.GONE);
                         answerBox.setVisibility(View.GONE);
-                        scoresTable.setVisibility(View.VISIBLE);
+                        relativeLayout.setVisibility(View.VISIBLE);
 
                         InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                         imm.hideSoftInputFromWindow(answerBox.getWindowToken(), 0);
+
+                        progressBar.setVisibility(View.GONE);
+                        closeLayout.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v){
+                                relativeLayout.setVisibility(View.GONE);
+                                submitButton.setVisibility(View.VISIBLE);
+                                answerBox.setVisibility(View.VISIBLE);
+                                progressBar.setVisibility(View.VISIBLE);
+                            }
+                        });
+
+                        submitNameButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String userInput = enterName.getText().toString();
+
+                                if (TextUtils.isEmpty(userInput)) {
+                                    Toast.makeText(GamePT2.this, "Please enter a name", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    relativeLayout.setVisibility(View.GONE);
+                                    scoresTable.setVisibility(View.VISIBLE);
+                                    for (int i=0; i<matrix.length; ++i) {
+                                        for (int j=0; j<matrix[i].length; ++j) {
+                                            matrix[i][j] = String.valueOf(i);
+                                            for(int k=0; k<5; k++) {
+                                                if (name1.getText()!="testing") {
+                                                    name1.setText(enterName.getText());
+                                                    number1.setText((String.valueOf(practiseGame.reqNum.getText())));
+                                                    time1.setText(Timer.getText());
+                                                    Date c = Calendar.getInstance().getTime();
+                                                    SimpleDateFormat df = new SimpleDateFormat("dd/MMM/yyyy", Locale.getDefault());
+                                                    String formattedDate = df.format(c);
+                                                    date1.setText(formattedDate);
+
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+
+                            }
+
+                        });
 
                     }
                 }
@@ -119,16 +219,7 @@ public class GamePT2 extends AppCompatActivity {
     }
 
 
-    public static void hideKeyboard(GamePT2 activity) {
-        InputMethodManager imm = (InputMethodManager) activity.getSystemService(GamePT2.INPUT_METHOD_SERVICE);
-        //Find the currently focused view, so we can grab the correct window token from it.
-        View view = activity.getCurrentFocus();
-        //If no view currently has focus, create a new one, just so we can grab a window token from it
-        if (view == null) {
-            view = new View(activity);
-        }
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
+
 
     private void startTimer() {
         countUpTimer = new CountDownTimer(Long.MAX_VALUE, 10) {
